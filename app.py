0f40bc79c33dc2b74f73c0205670e0267ffa4c82
Capriel2003 <gabrielbraganca20039@gmail.com>
2024-08-19 21:14:33 -0300

    get

--- app.py
+++ app.py
@@ -1,60 +1,87 @@
-from flask import Flask, Response
 import cv2 as cv
 import numpy as np
-
-app = Flask(__name__)
 
 def extrairMaiorCtn(img):
     imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     imgTh = cv.adaptiveThreshold(imgGray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 12)
     kernel = np.ones((2,2), np.uint8)
     imgDil = cv.dilate(imgTh, kernel)
-    contours, _ = cv.findContours(imgDil, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    contours, hi = cv.findContours(imgDil, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
-    if contours:
-        maiorCtn = max(contours, key=cv.contourArea)
-        x, y, w, h = cv.boundingRect(maiorCtn)
-        bbox = [x, y, w, h]
-        recorte = img[y:y+h, x:x+w]
-        recorte = cv.resize(recorte, (400, 750))
-    else:
-        bbox = [0, 0, 0, 0]
-        recorte = img
+    maiorCtn = max(contours, key=cv.contourArea)
+    x, y, w, h = cv.boundingRect(maiorCtn)
+    bbox = [x, y, w, h]
+    recorte = img[y:y+h, x:x+w]
+    recorte = cv.resize(recorte, (400, 750))
+    cv.imshow('Recorte', recorte)
 
     return recorte, bbox
 
-def generate_frames():
-    video = cv.VideoCapture(0)  # Captura a imagem da câmera
-    while True:
-        success, imagem = video.read()
-        if not success:
-            break
-        
-        imagem = cv.resize(imagem, (600, 700))
-        imgContours = imagem.copy()
-        gabarito, bbox = extrairMaiorCtn(imagem)
+# Defina as coordenadas e respostas diretamente no código
+campos = [
+    [50, 100, 30, 30],  # Exemplo de coordenadas [x, y, w, h]
+    [90, 100, 30, 30],
+    [130, 100, 30, 30],
+    # Adicione mais campos conforme necessário
+]
 
-        imgGray2 = cv.cvtColor(gabarito, cv.COLOR_BGR2GRAY)
-        imgGray = cv.cvtColor(imagem, cv.COLOR_BGR2GRAY)
-        imgBlur = cv.GaussianBlur(imgGray, (5,5), 1)
-        imgCanny = cv.Canny(imgBlur, 10, 50)
+resp = [
+    'A',  # Respostas correspondentes aos campos acima
+    'B',
+    'C',
+    # Adicione mais respostas conforme necessário
+]
 
-        ret, imgTh = cv.threshold(imgGray2, 70, 255, cv.THRESH_BINARY_INV)
-        contours, _ = cv.findContours(imgCanny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-        cv.drawContours(imgContours, contours, -1, (0, 255, 0), 2)
-        cv.rectangle(imagem, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0, 255, 0), 3)
+respostasCorretas = ["1-B", "2-A", "3-C", "4-C", "5-A", "6-A", "7-C", "8-B", "9-D", "10-A"]
 
-        # Codifica a imagem em formato JPEG e a converte para bytes
-        ret, buffer = cv.imencode('.jpg', imagem)
-        frame = buffer.tobytes()
+img = cv.imread("D:\\BKP_D\\Visual Studio Code\\Scanner_DC\\gab2_10_marcado.jpg")
 
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    video.release()
+img = cv.resize(img, (600, 700))
+imgContours = img.copy()
+gabarito, bbox = extrairMaiorCtn(img)
+imgGray2 = cv.cvtColor(gabarito, cv.COLOR_BGR2GRAY)
+imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+imgBlur = cv.GaussianBlur(imgGray, (5, 5), 1)
+imgCanny = cv.Canny(imgBlur, 10, 50)
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+ret, imgTh = cv.threshold(imgGray2, 70, 255, cv.THRESH_BINARY_INV)
+contours, h = cv.findContours(imgCanny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+cv.drawContours(imgContours, contours, -1, (0, 255, 0), 2)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+cv.rectangle(img, (bbox[0], bbox[1]), (bbox[0] + bbox[2], bbox[1] + bbox[3]), (0, 255, 0), 3)
+
+respostas = []
+for id, vg in enumerate(campos):
+    x = int(vg[0])
+    y = int(vg[1])
+    w = int(vg[2])
+    h = int(vg[3])
+    cv.rectangle(gabarito, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv.rectangle(imgTh, (x, y), (x + w, y + h), (255, 255, 255), 1)
+    campo = imgTh[y:y + h, x:x + w]
+    height, width = campo.shape[:2]
+    tamanho = height * width
+    brancas = cv.countNonZero(campo)
+    percentual = round((brancas / tamanho) * 100, 2)
+    if percentual >= 18:
+        cv.rectangle(gabarito, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        respostas.append(resp[id])
+
+print("Respostas:", respostas)
+
+erros = 0
+acertos = 0
+if len(respostas) == len(respostasCorretas):
+    for num, res in enumerate(respostas):
+        if res == respostasCorretas[num]:
+            acertos += 1
+        else:
+            erros += 1
+
+pontuacao = int(acertos * 10)
+cv.putText(img, f'ACERTOS: {acertos}, PORCENTAGEM: {pontuacao}%', (30, 140), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+cv.imshow('img', img)
+cv.imshow('Gabarito', gabarito)
+cv.imshow('IMG TH', imgTh)
+cv.waitKey(0)
